e5a37563ce2cbffdb835e57601854738be32bd53
Jude Baca-Miller <judeamiller@gmail.com>
2018-10-25 13:51:56 -0600

    move complete santa fe boundary to separate variable.

--- js/map.js
+++ js/map.js
@@ -1,11 +1,10 @@
 var map = L.map('mapid').setView([35.69,-105.94], 11);
 
-L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 }).addTo(map);
 
-L.geoJson(santafeEducationCensus).addTo(map);
+// L.geoJson(santafeEducationCensus).addTo(map);
 
 function getColor(d) {
 	return d > 40   ? '#00441b' :
@@ -30,4 +29,43 @@ function eduStyle(feature) {
 	};
 }
 
-L.geoJson(santafeEducationCensus, {style: eduStyle}).addTo(map);
+L.geoJson(santafeEducationCensus, {
+	style: eduStyle,
+	onEachFeature: function (feature, layer) {
+        layer.bindPopup(
+            "<h6>Educational Attainment</h6> <hr>" +
+			"<strong>Tract Population: </strong>" + feature.properties.B15003001 + '<br>' +
+			'<strong>Tract Name: </strong>' + feature.properties.name + '<br>'
+            );
+            layer.on('mouseover', function (e) {
+                // Add active class to selected element
+                layer.getElement().classList.add('active');
+            });
+            layer.on('mouseout', function (e) {
+                this.closePopup();
+                // Remove any previous instances of 'active' class from map
+                document.querySelectorAll('.active').forEach(function(element){
+                    element.classList.remove('active');
+				}); 
+			}); 
+		}
+}).addTo(map);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function(map) {
+	var div = L.DomUtil.create('div', 'info legend'),
+	grades = [0, 5, 10, 15, 20, 25, 30, 35, 40],
+	labels = [];
+
+	div.innerHTML += "<b>Percentage of population with <br> Bachelor's degree or higher</b><br>"
+
+	for (let i = 0;  i < grades.length; i++ ) {
+		div.innerHTML += 
+		'<i style="background: ' + getColor(grades[i] + 1) + '"></i> ' + grades[i] + (grades[i+1]  ? ' – ' + grades[i + 1] + '%' + "<br>" : '+');
+	}
+
+	return div;
+}
+
+legend.addTo(map);
